963c39664e39982ca4509460cf2a2c3e4920f763
Dilip <dilip45377@gmail.com>
2022-08-27 22:46:39 +0530

    Added new lines of Code

--- PrintIncreaseAndDecreaseOrder.cpp
+++ PrintIncreaseAndDecreaseOrder.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 
 void printIncreaseAndDecrease(int n, int countN){
+        if(countN <= 0){
+            return;
+        }
 
+        cout << n - countN + 1 << " ";
+
+        printIncreaseAndDecrease(n, countN-1);
+
+        cout << n - 1 << " ";
 }
 
 int main(){
