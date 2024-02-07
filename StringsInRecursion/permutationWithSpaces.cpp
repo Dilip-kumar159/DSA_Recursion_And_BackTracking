f972e0578038f1f94d1bee29786676c6fae2a748
Dilip <dilip45377@gmail.com>
2024-02-08 02:37:47 +0530

    add Permutation With Spaces

---
+++ StringsInRecursion/permutationWithSpaces.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+void permute(string &str, string output, int ind){
+
+    if(ind == str.length()){
+        cout<<output<<endl;
+        return;
+    }
+
+    // include spaces
+    permute(str, output + "_" +str[ind], ind+1);
+
+    // exclude spaces
+    permute(str, output + str[ind], ind+1);
+
+}
+int main(){
+
+    string str = "ABCD";
+
+    string output = "";
+
+    output += str[0];
+
+    permute(str, output, 1);
+
+
+    return 0;
+}
